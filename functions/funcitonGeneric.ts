5475059be0e36178c86ea220ce43224145eee9fd
sduzair <sduzairahmed@gmail.com>
2023-09-18 00:34:59 -0400

    function generic instead of overloads?

--- functions/funcitonGeneric.ts
+++ functions/funcitonGeneric.ts
@@ -1,5 +1,6 @@
 //? FUNCTION GENERIC ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
+// Example 1: Without type constraint
 function firstElement<Type>(arr: Type[]): Type | undefined {
     return arr[0];
 }
@@ -17,7 +18,7 @@ const a = firstElement(["a", 1, "c"]);
 const u = firstElement([]);
 
 
-// Example with type constraint
+// Example 2: With type constraint
 function longest<Type extends { length: number }>(a: Type, b: Type) {
     if (a.length >= b.length) {
         return a;
@@ -34,3 +35,23 @@ const longerString = longest("alice", "bob");
 
 // const notOK = longest(10, 100);      //! Error! Numbers don't have a 'length' property
 
+// Example 3: Replacement for function overloads?
+interface Box<Type> {
+    contents: Type;
+}
+
+function setContents<Type extends number | string>(box: Box<Type>, newContents: Type) {
+    box.contents = newContents;
+    if (typeof box.contents === "string") {
+        const a: string = box.contents;
+    }
+}
+
+function setContentsOverload(box: { contents: string }, newContents: string): void;
+function setContentsOverload(box: { contents: number }, newContents: number): void;
+function setContentsOverload(box: { contents: number } | { contents: string }, newContents: number | string): void {
+    box.contents = newContents;
+    if (typeof box.contents === "string") {
+        const a: string = box.contents;
+    }
+}
