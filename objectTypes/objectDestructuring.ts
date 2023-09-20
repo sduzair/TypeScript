13b6551de378f37b628b1e6a8656933fb101b62c
sduzair <sduzairahmed@gmail.com>
2023-09-19 20:48:53 -0400

    errors as comments

--- objectTypes/objectDestructuring.ts
+++ objectTypes/objectDestructuring.ts
@@ -20,13 +20,13 @@ function example2({ shape, xPos = 0, yPos = 0 }: PaintOpts) {
 // Example 3 - Object destructuring with renaming
 function example3({ shape: shp, xPos, yPos }: PaintOpts) {
     // ...
-    shape   //! Error! shape is not defined
+    // shape   //! Error! shape is not defined
 }
 
 // Example 4 - Object destructuring with renaming and default values
 function example4({ shape: shp, xPos: xPosition = 0, yPos: yPosition = 0 }: PaintOpts) {
     // ...
-    shape   //! Error! shape is not defined
-    xPos    //! Error! xPos is not defined
-    yPos    //! Error! yPos is not defined
+    // shape   //! Error! shape is not defined
+    // xPos    //! Error! xPos is not defined
+    // yPos    //! Error! yPos is not defined
 }
